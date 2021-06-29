e3325b102c086d8723268760a86fe5469928676e
mdskinner31 <mdskinner31@gmail.com>
2021-06-29 15:15:06 -0400

    Add project files.

---
+++ CoffeeFinder.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeFinder.WebMVC.Startup))]
+namespace CoffeeFinder.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
