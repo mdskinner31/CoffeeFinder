690d360200121f4162be110a6b8d5522facd310e
mdskinner31 <mdskinner31@gmail.com>
2021-07-09 18:03:23 -0400

    Menu class model service

---
+++ CoffeeFinder.Models/MenuDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeFinder.Models
+{
+    public class MenuDetail
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double MiRating { get; set; }
+
+        public bool IsRecommended { get; set; }
+    }
+}
